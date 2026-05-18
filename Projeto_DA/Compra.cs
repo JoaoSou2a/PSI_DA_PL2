8270bc012e3e6575b275501e1006c8bb316ae08e
davidfixolascachimbas <2025187687@my.ipleiria.pt>
2026-05-18 16:28:51 +0100

    ........ compra, itemcompra, itemprevisto e itemnaoprevisto.

---
+++ Projeto_DA/Compra.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_DA
+{
+    internal class Compra
+    {
+        public int id { get; set; }
+        public string nomeCompra { get; set; }
+        public DateTime dataCriacao { get; set; }
+        public DateTime dataFechada {  get; set; }
+        public bool fechada {  get; set; }
+        public decimal valorTotal { get; set; }
+        public DateTime dataAlteracao { get; set; }
+        public Utilizador utilizador { get; set; }
+    }
+}
---
+++ Projeto_DA/ItemCompra.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_DA
+{
+    internal class ItemCompra
+    {
+
+
+        public int quantidadeAdquirida {  get; set; }
+        public decimal precoUnitario { get; set; }
+    }
+}
