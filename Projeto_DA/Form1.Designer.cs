7d60a0df877f5193e0387391204198e98646049a
gustavo <2025187758@my.ipleiria.pt>
2026-05-18 14:58:12 +0100

    .

--- Projeto_DA/Form1.Designer.cs
+++ Projeto_DA/Form1.Designer.cs
@@ -2,16 +2,8 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Variável de designer necessária.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// ola
-        /// Limpar os recursos que estão sendo usados.
-        /// </summary>
-        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -21,12 +13,6 @@
             base.Dispose(disposing);
         }
 
-        #region Código gerado pelo Windows Form Designer
-
-        /// <summary>
-        /// Método necessário para suporte ao Designer - não modifique 
-        /// o conteúdo deste método com o editor de código.
-        /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
@@ -34,8 +20,5 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "Form1";
         }
-
-        #endregion
     }
 }
-
