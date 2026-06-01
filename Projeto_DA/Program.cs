1604dccd0f1004038a83fe03ca178fc45ca7aee6
João Sousa <2231654@my.ipleiria.pt>
2026-06-01 17:28:47 +0100

    Criar formulario principal

--- Projeto_DA/Program.cs
+++ Projeto_DA/Program.cs
@@ -29,7 +29,7 @@ namespace Projeto_DA
             //teste
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new PrincipalForm());
         }
     }
 }
